73010a4cc6133780a9d9d2105d7d8e574902aad9
Heucles <hpelegia@oito.srv.br>
2017-10-23 17:07:33 -0200

    Added mongodb persistence layer

--- src/main/java/com/howtographql/sample/GraphQLEndpoint.java
+++ src/main/java/com/howtographql/sample/GraphQLEndpoint.java
@@ -4,6 +4,8 @@ import com.coxautodev.graphql.tools.SchemaParser;
 import javax.servlet.annotation.WebServlet;
 
 import com.howtographql.sample.repository.LinkRepository;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
 import graphql.schema.GraphQLSchema;
 import graphql.servlet.SimpleGraphQLServlet;
 
@@ -11,12 +13,18 @@ import graphql.servlet.SimpleGraphQLServlet;
 @WebServlet(urlPatterns = "/graphql")
 public class GraphQLEndpoint extends SimpleGraphQLServlet {
 
+    private static final LinkRepository linkRepository;
+
+    static {
+        MongoDatabase mongo = new MongoClient("172.17.0.2", 27017).getDatabase("hackernews");
+        linkRepository = new LinkRepository(mongo.getCollection("links"));
+    }
+
     public GraphQLEndpoint() {
         super(buildSchema());
     }
 
     private static GraphQLSchema buildSchema() {
-        LinkRepository linkRepository = new LinkRepository();
         return SchemaParser.newParser()
                 .file("schema.graphqls")
                 .resolvers(new Query(linkRepository), new Mutation(linkRepository))
--- src/main/java/com/howtographql/sample/repository/LinkRepository.java
+++ src/main/java/com/howtographql/sample/repository/LinkRepository.java
@@ -1,26 +1,42 @@
 package com.howtographql.sample.repository;
 
 import com.howtographql.sample.model.Link;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class LinkRepository {
 
-    private final List<Link> links;
+    private final MongoCollection<Document> links;
 
-    public LinkRepository() {
-        links = new ArrayList<>();
-        //add some links to start off with
-        links.add(new Link("http://howtographql.com", "Your favorite GraphQL page"));
-        links.add(new Link("http://graphql.org/learn/", "The official docks"));
+    private Link link(Document doc) {
+        return new Link(
+                doc.get("_id").toString(),
+                doc.getString("url"),
+                doc.getString("description"));
+    }
+
+    public LinkRepository(MongoCollection<Document> links) {
+        this.links = links;
     }
 
     public List<Link> getAllLinks() {
-        return links;
+        List<Link> allLinks = new ArrayList<Link>();
+        for (Document doc :
+                this.links.find()) {
+            allLinks.add(link(doc));
+        }
+
+        return allLinks;
     }
 
     public void saveLink(Link link) {
-        links.add(link);
+        Document doc = new Document();
+        doc.append("url", link.getUrl());
+        doc.append("description", link.getDescription());
+        this.links.insertOne(doc);
     }
 }
